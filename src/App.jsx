5cfc87bc10b56622c82e91826a16dd5bd87430df
Shruti <143989366+tanugopi@users.noreply.github.com>
2024-01-03 16:36:15 +0530

    Creat my Hightlight's Time Line

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+import Time from './Timeline.jsx'
 
-const Timeline =() =>{
+const App =() =>{
   return(
     <div>
       <h1>Shruti's High-Lights</h1>
+      <Time year = {2005} event = "This year is my born year"/>
+      <Time year = {2009} event = "I am very injurd by my bycle"/>
+      <Time year = {2009} event = "We are going to my district lakhimpur kheri for studing"/>
+      <Time year = {2010} event = "In this I am got the first position in my K.G. class"/>
+      <Time year = {2012} event = "Bigest event in my district for 12/12/12 becouse 12/12/12 is never come"/>
+      <Time year = {2021} event = "This year I am selected in milaan for Girl Icon leader"/>
+      <Time year = {2022} event = "This year I am selected in milaan for Project Assistent"/>
     </div>
   )
 }
